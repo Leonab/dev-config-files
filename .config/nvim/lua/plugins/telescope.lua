37ca522f1e3a5e7cd412548d0e8b6958b2accdfd
Nabeel Khawar <leonabrocks@yahoo.com>
2025-10-19 22:36:41 +0100

    add fzf plugin to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,54 +1,57 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-		"nvim-telescope/telescope-symbols.nvim",
-	},
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
-		local builtin = require("telescope.builtin")
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope-symbols.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
-		telescope.setup({
-			defaults = {
-				path_display = { "truncate" },
-				preview = {
-					filesize_limit = 0.1, -- MB
-				},
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-					},
-				},
-			},
-		})
+    telescope.setup({
+      defaults = {
+        path_display = { "truncate" },
+        preview = {
+          filesize_limit = 0.1, -- MB
+        },
+        extensions = {
+          fzf = {},
+        },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+        },
+      },
+    })
 
-		telescope.load_extension("fzf")
+    telescope.load_extension("fzf")
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fsr", "<cmd>Telescope resume<cr>", { desc = "[R]epeat last search" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fsr", "<cmd>Telescope resume<cr>", { desc = "[R]epeat last search" })
 
-		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
+    keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
 
-		keymap.set("n", "<leader>fg", builtin.git_files, { desc = "[F]ind [G]it files" })
+    keymap.set("n", "<leader>fg", builtin.git_files, { desc = "[F]ind [G]it files" })
 
-		keymap.set("n", "<leader>fsk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    keymap.set("n", "<leader>fsk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 
-		keymap.set("n", "<leader>fsn", function()
-			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
+    keymap.set("n", "<leader>fsn", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "[S]earch [N]eovim files" })
 
-		keymap.set("n", "<leader>fy", "<cmd>Telescope symbols<cr>", { desc = "Find symbols/emojis" })
-	end,
+    keymap.set("n", "<leader>fy", "<cmd>Telescope symbols<cr>", { desc = "Find symbols/emojis" })
+  end,
 }
