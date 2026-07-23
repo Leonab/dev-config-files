5fb2cc3a238eea814267a3d5ddcd9299022116b8
Nabeel Khawar <leonabrocks@yahoo.com>
2026-07-23 22:23:20 +0100

    chore: added git blame keybind

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,16 +1,17 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-		on_attach = function(bufnr)
-			local gs = package.loaded.gitsigns
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
 
-			local function map(mode, l, r, desc)
-				vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-			end
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+      end
 
-			map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
-			map("n", "<leader>hd", gs.diffthis, "Diff this")
-		end,
-	},
+      map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
+      map("n", "<leader>hb", gs.blame, "Git Blame buffer")
+      map("n", "<leader>hd", gs.diffthis, "Git Diff this")
+    end,
+  },
 }
